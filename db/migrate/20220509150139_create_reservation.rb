9013f4f878dbb7b75af8ff815d628540e03de511
Richard Tonata <rtonata@gmail.com>
2022-05-09 18:33:08 +0200

    Fix migration errors

--- db/migrate/20220509150139_create_reservation.rb
+++ db/migrate/20220509150139_create_reservation.rb
@@ -1,9 +1,9 @@
 class CreateReservation < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :psychologist_id, null: false, foreign_key: true
-      t.references :city_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :psychologist, null: false, foreign_key: true
+      t.references :city, null: false, foreign_key: true
       t.datetime :reservation_date
 
       t.timestamps
