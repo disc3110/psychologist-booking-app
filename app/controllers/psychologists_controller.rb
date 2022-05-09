d474b941d7be24fc1648b7b58682113f541d09a7
Richard Tonata <rtonata@gmail.com>
2022-05-09 22:13:21 +0200

    Fix linters

--- app/controllers/psychologists_controller.rb
+++ app/controllers/psychologists_controller.rb
@@ -1,5 +1,5 @@
 class PsychologistsController < ApplicationController
-  before_action :set_psychologist, only: %i[ show update destroy ]
+  before_action :set_psychologist, only: %i[show update destroy]
 
   # GET /psychologists
   def index
@@ -39,13 +39,14 @@ class PsychologistsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_psychologist
-      @psychologist = Psychologist.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def psychologist_params
-      params.require(:psychologist).permit(:name, :gender, :email, :area_of_specialty, :image)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_psychologist
+    @psychologist = Psychologist.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def psychologist_params
+    params.require(:psychologist).permit(:name, :gender, :email, :area_of_specialty, :image)
+  end
 end
--- spec/requests/psychologists_spec.rb
+++ spec/requests/psychologists_spec.rb
@@ -12,116 +12,116 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/psychologists", type: :request do
+RSpec.describe '/psychologists', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Psychologist. As you add validations to Psychologist, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # PsychologistsController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Psychologist.create! valid_attributes
       get psychologists_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       psychologist = Psychologist.create! valid_attributes
       get psychologist_url(psychologist), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Psychologist" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Psychologist' do
+        expect do
           post psychologists_url,
                params: { psychologist: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Psychologist, :count).by(1)
+        end.to change(Psychologist, :count).by(1)
       end
 
-      it "renders a JSON response with the new psychologist" do
+      it 'renders a JSON response with the new psychologist' do
         post psychologists_url,
              params: { psychologist: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Psychologist" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Psychologist' do
+        expect do
           post psychologists_url,
                params: { psychologist: invalid_attributes }, as: :json
-        }.to change(Psychologist, :count).by(0)
+        end.to change(Psychologist, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new psychologist" do
+      it 'renders a JSON response with errors for the new psychologist' do
         post psychologists_url,
              params: { psychologist: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        skip('Add a hash of attributes valid for your model')
+      end
 
-      it "updates the requested psychologist" do
+      it 'updates the requested psychologist' do
         psychologist = Psychologist.create! valid_attributes
         patch psychologist_url(psychologist),
               params: { psychologist: new_attributes }, headers: valid_headers, as: :json
         psychologist.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "renders a JSON response with the psychologist" do
+      it 'renders a JSON response with the psychologist' do
         psychologist = Psychologist.create! valid_attributes
         patch psychologist_url(psychologist),
               params: { psychologist: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the psychologist" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the psychologist' do
         psychologist = Psychologist.create! valid_attributes
         patch psychologist_url(psychologist),
               params: { psychologist: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested psychologist" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested psychologist' do
       psychologist = Psychologist.create! valid_attributes
-      expect {
+      expect do
         delete psychologist_url(psychologist), headers: valid_headers, as: :json
-      }.to change(Psychologist, :count).by(-1)
+      end.to change(Psychologist, :count).by(-1)
     end
   end
 end
